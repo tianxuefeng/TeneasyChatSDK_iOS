fde0b38e89dde5ded4fbaab98d6f89a9f3a3922f
tianxuefeng <tianxuefeng2010@gmail.com>
2023-01-19 21:02:44 +0800

    Initial commit

---
+++ Example/TeneasyChatSDK_iOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TeneasyChatSDK_iOS
+//
+//  Created by 7938813 on 01/19/2023.
+//  Copyright (c) 2023 7938813. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
