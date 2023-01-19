91b38925088c29828e3c20ba79da11a41e9fef86
tianxuefeng <tianxuefeng2010@gmail.com>
2023-01-20 00:35:28 +0800

    add package dependence

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "TeneasyChatSDK_iOS",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "TeneasyChatSDK_iOS",
+            targets: ["TeneasyChatSDK_iOS"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0")
+    ],
+    targets: [
+        .target(
+            name: "TeneasyChatSDK_iOS",
+          
+            path: "TeneasyChatSDK_iOS"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
