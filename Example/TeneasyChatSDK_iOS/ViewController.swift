5f88471e804b7792ca1155c1c05c075794b78ec7
tianxuefeng <tianxuefeng2010@gmail.com>
2023-01-19 21:28:38 +0800

    添加测试函数

--- Example/TeneasyChatSDK_iOS/ViewController.swift
+++ Example/TeneasyChatSDK_iOS/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import TeneasyChatSDK_iOS
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let lib = libTest()
+        lib.toastHello(vc: self)
     }
 
     override func didReceiveMemoryWarning() {
